9230daa573bc331baee394dee06e50d2c95ad1f5
jdecee <jdecee@gmail.com>
2021-11-03 13:28:33 -0400

    First commit

---
+++ run.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+
+@app.route('/test')
+def test():
+    return 'Test'
+
+if __name__ == '__main__':
+    app.run()

9df57b5eeadc062c28598f90845ba877206e7b08
jdecee <jdecee@gmail.com>
2021-11-03 19:22:58 -0400

    Second commit

--- run.py
+++ run.py
@@ -1,17 +1,4 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-
-@app.route('/')
-def index():
-    return 'Hello World!'
-
-
-@app.route('/test')
-def test():
-    return 'Test'
+from app import app
 
 if __name__ == '__main__':
     app.run()

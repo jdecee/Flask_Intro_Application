91f0d44acae6ba454a00e7ae82e352aa8c50fad2
jdecee <jdecee@gmail.com>
2021-11-08 17:43:33 -0500

    added a posts form

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms import validators
 from wtforms.validators import DataRequired, Email, EqualTo
 
 
@@ -9,3 +10,8 @@ class UserInfoForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_pass = StringField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = StringField('Content'), validators=[DataRequired(), Email()]
+    submit = SubmitField()
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template
-from app.forms import UserInfoForm
+from app.forms import UserInfoForm, PostForm
+from app.models import User, Post
+from app import db
 
 @app.route('/')
 def index():
@@ -27,4 +29,18 @@ def register():
         username = register_form.username.data
         email = register_form.email.data
         password = register_form.password.data
+        new_user = User(username, email, password)
+        db.session.add(new_user)
+        db.session.commit()
     return render_template('register.html', form=register_form)
+
+@app.route('/createpost', methods=['GET', 'POST'])
+def createpost():
+    form = PostForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.content.data
+        new_post = Post(title, content, user_id)
+        db.session.add(post)
+        db.session.commit()
+    return render_template('createpost.html', form=form)

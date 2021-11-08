574c79578ed05e6e009f755a59bfe7c7d283216d
jdecee <jdecee@gmail.com>
2021-11-08 16:59:15 -0500

    added config settings for database

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from app import db
+import os
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), nullable=False, unique=True)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(256), nullable=False)
+
+    def __init__(self, username, email, password):
+        self.username=username
+        self.email = email
+        self.password = generate_password_hash(password)
+        
